ec878b0279fd9f32108894e13bac61d0d9af9291
Michael R. Krisnadhi <michaelkrisnadhi@gmail.com>
2021-10-29 13:53:49 +0700

    fix SQL queries

--- cmds_view.go
+++ cmds_view.go
@@ -191,11 +191,11 @@ func cmd_supplier_ranking() error {
     var data *sql.Rows
     var qb strings.Builder
 
-    qb.WriteString("SELECT S.KdSupplier, S.Nama, SUM(I.HargaJual * SMP.Stok) Nilai\n")
+    qb.WriteString("SELECT S.KdSupplier, S.Nama, ISNULL(SUM(I.HargaJual * SMP.Stok), 0) Nilai\n")
     qb.WriteString("FROM SUPPLIER S\n")
-    qb.WriteString("JOIN SUPPLIER_MENSUPLAI_PRODUK SMP ON SMP.KdSupplier = S.KdSupplier\n")
-    qb.WriteString("JOIN PRODUK PR ON PR.KdProduk = SMP.KdProduk\n")
-    qb.WriteString("JOIN IKAN I ON I.KdIkan = PR.KdIkan\n")
+    qb.WriteString("LEFT JOIN SUPPLIER_MENSUPLAI_PRODUK SMP ON SMP.KdSupplier = S.KdSupplier\n")
+    qb.WriteString("LEFT JOIN PRODUK PR ON PR.KdProduk = SMP.KdProduk\n")
+    qb.WriteString("LEFT JOIN IKAN I ON I.KdIkan = PR.KdIkan\n")
     qb.WriteString("GROUP BY S.KdSupplier, S.Nama\n")
     qb.WriteString("ORDER BY Nilai DESC")
     reportSQLStatement(qb.String())
@@ -282,11 +282,11 @@ func cmd_pabrik_ranking() error {
     var data *sql.Rows
     var qb strings.Builder
 
-    qb.WriteString("SELECT P.KdPabrik, P.Nama, SUM(I.HargaJual * PMP.Stok) Nilai\n")
+    qb.WriteString("SELECT P.KdPabrik, P.Nama, ISNULL(SUM(I.HargaJual * PMP.Stok), 0) Nilai\n")
     qb.WriteString("FROM PABRIK P\n")
-    qb.WriteString("JOIN PABRIK_MENGHASILKAN_PRODUK PMP ON PMP.KdPabrik = P.KdPabrik\n")
-    qb.WriteString("JOIN PRODUK PR ON PR.KdProduk = PMP.KdProduk\n")
-    qb.WriteString("JOIN IKAN I ON I.KdIkan = PR.KdIkan\n")
+    qb.WriteString("LEFT JOIN PABRIK_MENGHASILKAN_PRODUK PMP ON PMP.KdPabrik = P.KdPabrik\n")
+    qb.WriteString("LEFT JOIN PRODUK PR ON PR.KdProduk = PMP.KdProduk\n")
+    qb.WriteString("LEFT JOIN IKAN I ON I.KdIkan = PR.KdIkan\n")
     qb.WriteString("GROUP BY P.KdPabrik, P.Nama\n")
     qb.WriteString("ORDER BY Nilai DESC")
     reportSQLStatement(qb.String())
@@ -305,10 +305,10 @@ func cmd_petani_ranking() error {
     var data *sql.Rows
     var qb strings.Builder
 
-    qb.WriteString("SELECT P.KdPetani, P.Nama, SUM(I.HargaJual * PMI.StokDewasa) Nilai\n")
+    qb.WriteString("SELECT P.KdPetani, P.Nama, ISNULL(SUM(I.HargaJual * PMI.StokDewasa), 0) Nilai\n")
     qb.WriteString("FROM PETANI P\n")
-    qb.WriteString("JOIN PETANI_MEMBUDIDAYAKAN_IKAN PMI ON PMI.KdPetani = P.KdPetani\n")
-    qb.WriteString("JOIN IKAN I ON I.KdIkan = PMI.KdIkan\n")
+    qb.WriteString("LEFT JOIN PETANI_MEMBUDIDAYAKAN_IKAN PMI ON PMI.KdPetani = P.KdPetani\n")
+    qb.WriteString("LEFT JOIN IKAN I ON I.KdIkan = PMI.KdIkan\n")
     qb.WriteString("GROUP BY P.KdPetani, P.Nama\n")
     qb.WriteString("ORDER BY Nilai DESC")
     reportSQLStatement(qb.String())
@@ -382,10 +382,10 @@ func cmd_nelayan_ranking() error {
     var data *sql.Rows
     var qb strings.Builder
 
-    qb.WriteString("SELECT N.KdNelayan, N.Nama, SUM(I.HargaJual * NMI.Persediaan) Nilai\n")
+    qb.WriteString("SELECT N.KdNelayan, N.Nama, ISNULL(SUM(I.HargaJual * NMI.Persediaan), 0) Nilai\n")
     qb.WriteString("FROM NELAYAN N\n")
-    qb.WriteString("JOIN NELAYAN_MENANGKAP_IKAN NMI ON NMI.KdNelayan = N.KdNelayan\n")
-    qb.WriteString("JOIN IKAN I ON I.KdIkan = NMI.KdIkan\n")
+    qb.WriteString("LEFT JOIN NELAYAN_MENANGKAP_IKAN NMI ON NMI.KdNelayan = N.KdNelayan\n")
+    qb.WriteString("LEFT JOIN IKAN I ON I.KdIkan = NMI.KdIkan\n")
     qb.WriteString("GROUP BY N.KdNelayan, N.Nama\n")
     qb.WriteString("ORDER BY Nilai DESC")
     reportSQLStatement(qb.String())
